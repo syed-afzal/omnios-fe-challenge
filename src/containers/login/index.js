8c84373093b15353bd18dec037e29fe6de8757b1
Syed Afzal <sah.afzal@gmail.com>
2022-10-31 14:13:44 +0500

    Prettier is configured

--- src/containers/login/index.js
+++ src/containers/login/index.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import {APP, SIGN_UP} from '../../utils/constants';
-import {setToLocalStorage} from '../../utils/browser-storage';
-import {useHistory} from 'react-router-dom';
-import {Button, Row, Col, Form, ConfigProvider, Input} from 'antd';
-import {LockOutlined, UserOutlined} from '@ant-design/icons';
-import logo from '../../logo.png'
-import './login.scss';
+import React, {useState, useEffect} from "react";
+import {APP, SIGN_UP} from "../../utils/constants";
+import {setToLocalStorage} from "../../utils/browser-storage";
+import {useHistory} from "react-router-dom";
+import {Button, Row, Col, Form, ConfigProvider, Input} from "antd";
+import {LockOutlined, UserOutlined} from "@ant-design/icons";
+import logo from "../../logo.png";
+import "./login.scss";
 
 const {Item: FormItem} = Form;
 
 function Login() {
   const history = useHistory();
   const [form] = Form.useForm();
+  const [isDisabled, setIsDisabled] = useState(true);
 
   async function handleSubmit() {
     let values = {};
-    values = form.getFieldsValue(['email', 'password']);
+    values = form.getFieldsValue(["email", "password"]);
     console.log(values);
-    setToLocalStorage('user', values);
+    setToLocalStorage("user", values);
     history.push(APP);
   }
 
@@ -25,8 +26,21 @@ function Login() {
     history.push(SIGN_UP);
   };
 
-  return (
-    <ConfigProvider direction={'ltr'}>
+  const isFormValidValues = (from, keys) => {
+    const errors = form.getFieldsError(keys);
+    console.log(errors);
+    return true;
+  };
+
+  const handleFieldChange = () => {
+    setIsDisabled(true);
+    const keys = ["email", "password"];
+    if (!Object.values(form.getFieldsValue(keys)).some((v) => !v) && isFormValidValues(form, keys)) {
+      setIsDisabled(false);
+    }
+  };
+
+  return (<ConfigProvider direction={"ltr"}>
       <Row align="middle" justify="center" className="full-page">
         <Col span={24} className="login-container authenticate">
           <img src={logo} className="logo" alt="Omnios Logo"/>
@@ -36,14 +50,15 @@ function Login() {
             wrapperCol={{span: 24}}
             form={form}
             layout="vertical"
+            onFieldsChange={handleFieldChange}
           >
             <FormItem
               hasFeedback
               label="Email"
               name="email"
-              rules={[
-                {type: 'email', message: 'Please enter a valid email address'},
-              ]}
+              rules={[{required: true, message: "Email field is mandatory"}, {
+                type: "email", message: "Please enter a valid email address",
+              },]}
             >
               <Input
                 prefix={<UserOutlined className="login-form-icon"/>}
@@ -57,11 +72,11 @@ function Login() {
               hasFeedback
               name="password"
               label="Password"
-              rules={[
-                {required: true, message: 'Password field is mandatory'},
-                {min: 2, message: 'Password accepts a minimum of 2 characters'},
-                {max: 255, message: 'Password accepts a maximum of 255 characters'},
-              ]}
+              rules={[{required: true, message: "Password field is mandatory"}, {
+                min: 2, message: "Password accepts a minimum of 2 characters",
+              }, {
+                max: 255, message: "Password accepts a maximum of 255 characters",
+              },]}
             >
               <Input.Password
                 prefix={<LockOutlined className="login-form-icon"/>}
@@ -79,6 +94,7 @@ function Login() {
             className="login-form-button"
             htmlType="submit"
             onClick={handleSubmit}
+            disabled={isDisabled}
           >
             Log in
           </Button>
@@ -89,8 +105,7 @@ function Login() {
           </div>
         </Col>
       </Row>
-    </ConfigProvider>
-  );
+    </ConfigProvider>);
 }
 
 export default Login;
