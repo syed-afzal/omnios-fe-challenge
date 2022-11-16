85cc2795fc07ca1af59d60512d59db1ad4aeb2e1
Syed Afzal <sah.afzal@gmail.com>
2022-11-16 17:53:22 +0500

    Redirection issue resolved :zap:

--- src/router/privateRoute/index.js
+++ src/router/privateRoute/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { getFromLocalStorage } from '../../utils/browser-storage';
-import { LOGIN } from '../../utils/constants';
+import { LOGIN, HOME } from '../../utils/constants';
 
 function PrivateRoute(props) {
   const token = getFromLocalStorage('user');
   if (!token) {
     return <Redirect to={{ pathname: LOGIN }} />;
   }
+
+  if (props.location.pathname === '/') return <Redirect to={{ pathname: HOME }} />
   return <Route {...props} />;
 }
 
