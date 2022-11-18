bccbfba0a6bb160db3c860ccd7387a98ce2e0660
Syed Afzal <sah.afzal@gmail.com>
2022-11-18 15:07:34 +0500

    Custom Hook :zap:

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,18 +1,26 @@
 import React, { Fragment, useState, useRef, useEffect } from 'react';
 import './home.scss';
 
+function usePrevious(count) {
+  const prevCount = useRef();
+  useEffect(() => {
+    prevCount.current = count;
+  });
+  return prevCount.current;
+}
+
 function Home() {
   const [counter, setCounter] = useState(0);
-  const prevCounterRef = useRef();
+  const prevCounter = usePrevious(counter);
 
-  useEffect(() => {
-    prevCounterRef.current = counter;
-  }, [counter]);
+  // useEffect(() => {
+  //   prevCounterRef.current = counter;
+  // }, [counter]);
 
   return (
     <Fragment>
       <div className="main-class">
-        <h1>Counter Value With Ref: {prevCounterRef.current}</h1>
+        <h1>Counter Value With Ref: {prevCounter}</h1>
         <h1>Counter Value Actual: {counter}</h1>
         <button onClick={() => setCounter(counter + 1)}>Increment Counter</button>
       </div>
